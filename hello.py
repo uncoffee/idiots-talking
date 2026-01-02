5ee642b349bfebbf71e7580191202c2c0eb7670e
uncoffee <souta20080518@gmail.com>
2026-01-02 05:24:52 +0000

    とりま、せっとあっぷおわった

---
+++ hello.py
@@ -0,0 +1,397 @@
+import pymysql  #pip は a.txt
+
+from flask import Flask , render_template,request , redirect, url_for
+
+
+
+app = Flask(__name__)
+    
+def getconnection():#データベースへ接続
+    connection = pymysql.connect(
+    host="localhost", #わからん
+    db="mydb", #使ってるデータベースの名前
+    user="root", #わからん
+    password="", #パスワードは無し
+    charset="utf8", #文字コード指定
+    cursorclass=pymysql.cursors.DictCursor #dict型(辞書型)に型を変更している　これによってhtml上でのデータの取り出しが楽になる。らしい
+    )
+
+    return connection
+    
+    
+    
+@app.route("/result" , methods = ["post"])
+def result():
+    name = request.form["name"]
+    
+    connection = getconnection()
+    sql = "INSERT INTO players (name) VALUES (%s)"
+    cursor = connection.cursor()
+    cursor.execute(sql,name)
+    connection.commit()
+    
+    cursor.close()
+    connection.close()
+    
+    return redirect("/", code=302)
+    
+@app.route("/login" , methods = ["post"])
+def login():
+    
+    print("ログイン関数がたたかれたよ")
+    user_name = request.form["user_name"]
+    user_pass = request.form["user_pass"]
+    
+    print(f"\nuser_name:{user_name} user_pass:{user_pass}")
+    
+    #-----DBに接続します。------
+    connection = getconnection()
+    cursor = connection.cursor()
+    #---------------------------
+    
+    if user_name == "ad" and user_pass == "ad":
+        print("\n管理者　認証完了")
+        sql = "SHOW TABLES;"
+
+        cursor.execute(sql)
+        tables = cursor.fetchall()
+        
+        alldata = {}
+        
+        for table in tables:
+            print(table)
+            table_name = table['Tables_in_mydb']
+            table_name = table_name.strip("'")
+            
+            print(f"\nshow columns from {table_name};")
+            sql = "show columns from {table_name};"
+            cursor.execute(sql)
+            columns = cursor.fetchall()
+            
+            cursor_list = []
+            print(columns)
+            
+            for i in columns:
+                column_list = list(i.values())
+                cursor_list.append(column_list[0])
+                
+            alldata.update({str(table_name):cursor_list})
+            
+        #-----接続を解除します------
+        connection.commit()
+        cursor.close()
+        connection.close()
+        #---------------------------
+        
+        print(alldata)
+        return render_template("administrator.html" , alldata = alldata)
+        
+    #-------------------------------------------------------------------------
+    else:
+        print("\n通常のユーザ認証")
+        user_pass = int(user_pass) #文字の形をdbに合わせる。必要か？これ
+        
+        print(f"\nuser_name:{user_name}")
+        
+        sql = "SELECT user_id FROM informations WHERE user_name = %s;"
+        cursor.execute(sql , user_name)
+        user_id = cursor.fetchall()
+        
+        if user_id: #auto_incrementが0から始まる場合は死ぬよ♡
+            user_id = user_id[0]["user_id"] #返り血から欲しい値を取り出す。
+            print(f"\nuser_id:{user_id}") 
+            
+            sql = "SELECT user_id FROM passwords WHERE (user_id = %s) AND (password = %s);"
+            cursor.execute(sql , (user_id , user_pass))
+            check_account = cursor.fetchall()
+            
+            if check_account:#もし入力された値に該当するアカウントがあれば変数の中にuser_idが入る。なければ、0の値。
+                print(f"\n認証完了 id:{user_id} name:{user_name} pass:{user_pass}")
+                
+                sql = "SELECT friend_id FROM relations WHERE user_id = %s;"
+                cursor.execute(sql,user_id)
+                my_relation = cursor.fetchall()
+                
+                sql = "SELECT user_id FROM relations WHERE friend_id = %s;" 
+                cursor.execute(sql,user_id)
+                others_relation = cursor.fetchall()
+                
+                if others_relation: #そもそもフレンド申請が来ているかを確認する。
+                    print("\nお友達いるみたいです。よかったね")
+                    print(f"\nmy_relation:{my_relation}")
+                    print(f"\nothers_relation:{others_relation}")
+                    
+                    my_li = []
+                    oth_li = []
+                    
+                    request_list = []
+                    friend_list = []
+                    
+                    for i in my_relation:
+                        my_li.append(i["friend_id"])
+                        
+                    for i in others_relation:
+                        oth_li.append(i["user_id"])
+                        
+                    friend_list = set(oth_li) & set(my_li)
+                    request_list = set(oth_li) - set(my_li)
+                    
+                    print(f"\nfriend_list:{friend_list}")
+                    print(f"\nrequest_list:{request_list}")
+                    
+                    if friend_list:
+                        tuple(friend_list)
+                        print(friend_list)
+                        sql = "SELECT user_id , user_name FROM informations WHERE user_id in %s;" 
+                        cursor.execute(sql,friend_list)
+                        friend_list = cursor.fetchall()
+                    
+                    if request_list:
+                        tuple(request_list)
+                        print(request_list)
+                        sql = "SELECT user_id , user_name FROM informations WHERE user_id in %s;" 
+                        cursor.execute(sql,request_list)
+                        request_list = cursor.fetchall()
+                        
+                    print(f"\nmy_relation:{my_relation}")
+                    print(f"\nothers_relation:{others_relation}")
+                    print(f"\nrequest_list:{request_list}")
+                    print(f"\nfriend_list:{friend_list}")
+                    
+                    #-----接続を解除します------
+                    connection.commit()
+                    cursor.close()
+                    connection.close()
+                    #---------------------------
+                    
+                    return render_template("friends.html", user_name = user_name , user_id = user_id , friend_list = friend_list , request_list = request_list)
+                    
+                else:
+                    #一応求められてるから、変数を作成する。
+                    friend_list = []
+                    request_list = []
+                    
+                    print("\nフレンドもないし、申請もない。")
+                    return render_template("friends.html", user_name = user_name , user_id = user_id , friend_list = friend_list , request_list = request_list)
+                    
+            
+    
+    cursor.close()
+    connection.close()
+    return redirect("/", code=302)
+    
+    
+
+    
+    
+#開発中。    
+@app.route("/getchatlog" , methods = ["get"])
+def chatlog():
+    user_id = request.form["user_id"]
+    friend_id = request.form["friend_id"]
+    
+    #-----DBに接続します。------
+    connection = getconnection()
+    cursor = connection.cursor()
+    #---------------------------
+    
+    sql = "SELECT log_id , timelog , chatlog FROM chatlogs WHERE sender_id = %(user_id)s AND recelver_id = %(friend_id)s;"
+    
+    cursor.execute(sql , {"user_id":user_id , "friend_id":friend_id})
+    send_logs = cursor.fetchall()
+    
+    
+    sql = "SELECT log_id , timelog , chatlog FROM chatlogs WHERE sender_id = %(friend_id)s AND recelver_id = %(user_id)s;"
+    
+    cursor.execute(sql , {"user_id":user_id , "friend_id":friend_id})
+    recelver_logs = cursor.fetchall()
+    
+    chatlogs = []
+    
+    for send in send_logs:
+        send["sender"] = True
+        chatlogs.append(send)
+        
+    for recelver in recelver_logs:
+        recelver["sender"] = False
+        chatlogs.append(recelver)
+        
+    chatlogs = sorted(chatlog ,key=lambda logdata: logdata["log_id"])
+    
+    #-----接続を解除します------
+    connection.commit()
+    cursor.close()
+    connection.close()
+    #---------------------------
+    
+    #chatlogsには{"sender_id":"???","recelver_id":"???","timelog":"????y??m??d","chatlog":"?????????","sender":"True OR False"}　が入っている
+    return render_template("chatroom.html", chatlogs = chatlogs , user_name = user_name , friend_name = friend_name)
+        
+        
+@app.route("/friend_serch" , methods = ["post"])
+def friend_request():
+    
+    print("フレンド申請関数が叩かれたよ。")
+    
+    #リダイレクト難しいから値を取得する。
+    user_name = request.form["user_name"]
+    friend_list = request.form["friend_list"]
+    request_list = request.form["request_list"]
+    
+    #フレンド認証に使う値
+    user_id = int(request.form["user_id"])
+    request_id = int(request.form["request_id"])
+    
+    print(f"\nuser_id:{user_id} request_id:{request_id}")
+    
+    #-----DBに接続します。------
+    connection = getconnection()
+    cursor = connection.cursor()
+    #---------------------------
+
+    sql = "SELECT * FROM relations WHERE (user_id = %s AND friend_id = %s);"
+    result = cursor.execute(sql , (user_id , request_id)) #相手と自分の関係性を確認
+    
+    #戻り値が「0」であれば関係がない。「typeがlist型」であれば、フレンド申請済みもしくは、フレンド登録済み。
+    if result == 0:
+        sql = "INSERT INTO relations (user_id , friend_id) VALUES (%s , %s);"
+        result = cursor.execute(sql , (user_id , request_id))
+        
+        print(f"申請完了 相手のID:{request_id}")
+            
+    else:
+        print("不正なリクエスト。分岐近くのコメントを参照。")
+        
+    print("-" * 50)
+    
+    #-----接続を解除します------
+    connection.commit()
+    cursor.close()
+    connection.close()
+    #---------------------------
+    
+    return redirect("/", code=302)
+    
+    
+        
+@app.route("/friend_judg" , methods = ["post"])
+def friend_judg():
+    print("フレンド判断関数が叩かれたよ。")
+    
+    user_id = int(request.form["user_id"])
+    request_id = int(request.form["request_id"])
+    judg = request.form["judg"]
+    
+    print(f"\nuser_id:{user_id} request_id:{request_id}")
+    
+    #-----DBに接続します。------
+    connection = getconnection()
+    cursor = connection.cursor()
+    #---------------------------
+
+    sql = "SELECT * FROM relations WHERE user_id = %s AND friend_id = %s;"
+    result = cursor.execute(sql , (request_id , user_id)) #相手と自分の関係性を確認
+    
+    print("\n注意 relation テーブルには主キーしか含まれていないため、SELECT文に対してレコードの数で返してくるよ。")
+    print(f"関係性を確認。0であれば関係がない。listであれば関係あり。例外はしらん。:{result}")
+    
+    if result == 0:
+        print("\n不正なリクエスト。関係性が存在していないよ。")
+        
+    elif result == 1:
+        print("\n重複なしリクエストを確認。")
+        
+        if judg == "TRUE":
+            print(f"id:{request_id}さんからのリクエストを受けたよ")
+            sql = "INSERT INTO relations (user_id , friend_id) VALUES (%s , %s);"
+            cursor.execute(sql , (user_id , request_id))
+            
+        else:
+            print(f"id:{request_id}さんからのリクエストを断ったよ")
+            sql = "DELETE FROM relations WHERE user_id = %s AND friend_id = %s;"
+            cursor.execute(sql , (request_id , user_id))
+        
+    else:
+        print("多分もうフレンドになってると思うよ。")
+        
+    print("-" * 50)
+    
+    #-----接続を解除します------
+    connection.commit()
+    cursor.close()
+    connection.close()
+    #---------------------------
+    
+    return redirect("/", code=302)
+    
+@app.route("/make_account" , methods = ["post"])
+def make_account():
+    print("make_account関数がたたかれたよ！")
+    new_name = request.form["name"]
+    new_pass = int(request.form["pass"])
+    
+    print(f"\nnew_name:{new_name} new_pass:{new_pass}")
+    
+
+    
+    #新規作成のルールに則っているか確認。　ルール：名前の重複なし、かつ名前は１０文字まで。
+    
+        
+    if len(new_name) > 10:
+        print("\n名前が11文字以上です。")
+        return render_template("make_account.html" , error = 1)
+        
+        
+    #-----DBに接続します。------
+    connection = getconnection()
+    cursor = connection.cursor()
+    #---------------------------
+    
+    sql = "SELECT * FROM informations WHERE user_name = %s;"
+    result = cursor.execute(sql , new_name) #同じ名前のユーザがいないか確認
+    print(f"\nresult:{result}")
+    
+
+    if not result == 0: 
+        print("\n同じ名前のユーザーがいます。")
+        #-----接続を解除します------
+        cursor.close()
+        connection.close()
+        #---------------------------
+        return render_template("make_account.html" , error = 2)
+        
+    #---------------------------------------------------------------------------------------
+    
+    print("\n入力されたデータにもんだいはなかったよん")
+        
+    sql = "INSERT INTO informations (user_name) VALUES (%s);"
+    
+    cursor.execute(sql , new_name) #まずは名前からDBに入れる
+    new_id = cursor.lastrowid
+
+    print(f"\nnew_id:{new_id}")
+    
+    sql = "INSERT INTO passwords VALUES (%s , %s);"
+    
+    result = cursor.execute(sql , (new_id , new_pass)) #まずは名前からDBに入れる
+    
+    print(f"\n新規アカウント作成完了。 名前:{new_name} ID:{new_id} Pass:{new_pass}")
+    #-----接続を解除します------
+    connection.commit()
+    cursor.close()
+    connection.close()
+    #---------------------------
+    return render_template("login.html")
+    
+            
+@app.route("/make_account_html")
+def getlink():
+
+    return render_template("make_account.html" , error = 0)
+        
+@app.route("/")
+def hello_world():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run()
